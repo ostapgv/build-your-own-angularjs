a4aaa5e598dd9af02a075d1ccf225d719344c12d
Grigory Ostapenko <gmostapgv@gmail.com>
2015-09-03 12:45:14 +0200

    'scopes and digest' chapter part

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,6 +29,7 @@ module.exports = function(grunt) {
 						'test/**/*.js',
 						'node_modules/lodash/index.js',
 						'node_modules/jquery/dist/jquery.js',
+						'node_modules/angular/angular.js',
 						'node_modules/sinon/pkg/sinon.js'
 					],
 					watch_files: [
--- test/hello_spec.js
+++
@@ -1,9 +0,0 @@
-
-describe("Hello", function() {
-	
-	it("says hello to receiver", function() {
-		expect(sayHello('Jane')).toBe("Hello, Jane!");
-		expect(sayHello('Grigory')).toBe("Hello, Grigory!");
-	});
-	
-});
