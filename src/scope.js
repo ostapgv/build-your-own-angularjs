75b7f0db33937732769602f236c37d5b590acf8d
Grigory Ostapenko <gmostapgv@gmail.com>
2015-09-07 14:59:19 +0200

    The part 'Scopes' is finished

--- src/scope.js
+++ src/scope.js
@@ -1,4 +1,5 @@
 /* jshint globalstrict: true */
+/* global _ */
 'use strict';
 
 function initWatchVal() { }
@@ -12,6 +13,7 @@ function Scope() {
 	this.$$postDigestQueue = [];
 	this.$root = this;
 	this.$$children = [];
+	this.$$listeners = {};
 	this.$$phase = null;
 }
 
@@ -243,6 +245,7 @@ Scope.prototype.$new = function(isolated, parent) {
 	}
 	parent.$$children.push(child);
 	child.$$watchers = [];
+	child.$$listeners = {};
 	child.$$children = [];
 	child.$parent = parent;
 	return child;
@@ -265,6 +268,7 @@ Scope.prototype.$destroy = function() {
 	var siblings = this.$parent.$$children;
 	var indexOfThis = siblings.indexOf(this);
 	if (indexOfThis >= 0) {
+		this.$broadcast('$destroy');
 		siblings.splice(indexOfThis, 1);
 	}
 };
@@ -355,4 +359,77 @@ Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
 	};
 	
 	return this.$watch(internalWatchFn, internalListenerFn);
+};
+
+Scope.prototype.$on = function(eventName, listener) {
+	var listeners = this.$$listeners[eventName];
+	if (!listeners) {
+		this.$$listeners[eventName] = listeners = [];
+	}
+	listeners.push(listener);
+	return function() {
+		var index = listeners.indexOf(listener);
+		if (index >= 0) {
+			listeners[index] = null;
+		}
+	};
+};
+
+Scope.prototype.$emit = function(eventName) {
+	var propagationStopped = false;
+	var event = {
+		name: eventName, 
+		targetScope: this,
+		stopPropagation: function() {
+			propagationStopped = true;
+		},
+		preventDefault: function() {
+			event.defaultPrevented = true;
+		}
+	};
+	var listenerArgs = [event].concat(_.rest(arguments));
+	var scope = this;
+	do {
+		event.currentScope = scope;
+		scope.$$fireEventOnScope(eventName, listenerArgs);
+		scope = scope.$parent;
+	} while (scope && !propagationStopped);
+	event.currentScope = null;
+	return event;
+};
+
+Scope.prototype.$broadcast = function(eventName) {
+	var event = {
+		name: eventName,
+		targetScope: this,
+		preventDefault: function() {
+			event.defaultPrevented = true;
+		}
+	};
+	var listenerArgs = [event].concat(_.rest(arguments));
+	this.$$everyScope(function(scope) {
+		event.currentScope = scope;
+		scope.$$fireEventOnScope(eventName, listenerArgs);
+		return true;
+	});
+	event.currentScope = null;
+	return event;
+};
+
+Scope.prototype.$$fireEventOnScope = function(eventName, listenerArgs) {
+	var listeners = this.$$listeners[eventName] || [];
+	var i = 0;
+	while (i < listeners.length) {
+		if (listeners[i] === null) {
+			listeners.splice(i, 1);
+		} else {
+			try {
+				listeners[i].apply(null, listenerArgs);
+			} catch (e) {
+				console.error(e);
+			}
+			i++;
+		}
+	}
+	return event;
 };
